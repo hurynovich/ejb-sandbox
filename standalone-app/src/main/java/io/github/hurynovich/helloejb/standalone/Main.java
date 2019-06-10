896f203dc9a0ebde4e394dc0f754a28644e9d58e
Pavel Hurynovich <pavel.gurinovich@gmail.com>
2019-06-10 17:45:12 +0300

    created initial class

---
+++ standalone-app/src/main/java/io/github/hurynovich/helloejb/standalone/Main.java
@@ -0,0 +1,10 @@
+package io.github.hurynovich.helloejb.standalone;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        log.info("Log message.");
+    }
+}
